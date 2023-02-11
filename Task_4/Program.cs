fad02401a009d9a0de0ab21edab0cbb64c8b4f35
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-11 21:38:53 +0600

    Задача 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i < number + 1)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+        i = i + 2;
+    }
+    else
+    {
+        i = i + 2;
+    }
+}
