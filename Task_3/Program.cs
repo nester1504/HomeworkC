c220360eab55201be7f914829d719c04ec220e92
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-11 21:28:01 +0600

    Задача 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечётное");
+}
