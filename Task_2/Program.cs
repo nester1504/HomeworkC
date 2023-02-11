79a800cd3c15bda4b0359cca3dfd396d10f4a142
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-11 21:05:28 +0600

    Задача 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2 & number1 > number3)
+{
+    Console.WriteLine("Число " + number1 + " самое большое");
+}
+else if (number2 > number1 & number2 > number3)
+{
+    Console.WriteLine("Число " + number2 + " самое большое");
+}
+else
+{
+    Console.WriteLine("Число " + number3 + " самое большое");
+}
